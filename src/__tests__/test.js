ec597d761154496dea81ec71771db843878f6ad2
Andrey <symonany@gmail.com>
2022-12-31 19:14:01 +0300

    first commit

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,22 +1,177 @@
-import Validator from '../js/Validator';
-import formatingTelNumber from '../js/formatingTelNumber';
+import Team from '../js/set';
+import ErrorRepository from '../js/map';
 
-test.each([
-  ['User1111', false],
-  ['1-_Tester_-1', false],
-  ['Test_true', true],
-  ['Helown-_111u', true],
-])('testing validateUserName function with userName %s', (userName, expected) => {
-  const name = new Validator();
-  const result = name.validateUserName(userName);
-  expect(result).toBe(expected);
+test('testing Team.add function', () => {
+  const team = new Team();
+  const char = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  };
+
+  const expected = [
+    {
+      name: 'мечник',
+      health: 10,
+      level: 2,
+      attack: 80,
+      defence: 40,
+    },
+  ];
+
+  team.add(char);
+  const result = [];
+  team.members.forEach((item) => result.push(item));
+
+  expect(result).toEqual(expected);
 });
 
-test.each([
-  ['8 (927) 000-00-00', '+79270000000'],
-  ['+7 960 000 00 00', '+79600000000'],
-  ['+86 000 000 0000', '+860000000000'],
-])('testing formatingTelNumber function with name %s and %i health', (telNumber, expected) => {
-  const result = formatingTelNumber(telNumber);
-  expect(result).toBe(expected);
+test('testing Team.add error', () => {
+  const team = new Team();
+  const char = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  };
+
+  function testingError() {
+    return team.add(char);
+  }
+
+  team.add(char);
+
+  expect(testingError).toThrow('Персонаж');
+});
+
+test('testing Team.addAll function', () => {
+  const team = new Team();
+  const char1 = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  };
+  const char2 = {
+    name: 'лучник',
+    health: 8,
+    level: 2,
+    attack: 180,
+    defence: 20,
+  };
+  const char3 = {
+    name: 'маг',
+    health: 9,
+    level: 2,
+    attack: 160,
+    defence: 10,
+  };
+  const chars = [
+    char1,
+    char2,
+    char3,
+  ];
+  const expected = [
+    {
+      name: 'мечник',
+      health: 10,
+      level: 2,
+      attack: 80,
+      defence: 40,
+    },
+    {
+      name: 'лучник',
+      health: 8,
+      level: 2,
+      attack: 180,
+      defence: 20,
+    },
+    {
+      name: 'маг',
+      health: 9,
+      level: 2,
+      attack: 160,
+      defence: 10,
+    },
+  ];
+
+  team.addAll(chars);
+  const result = [];
+  team.members.forEach((item) => result.push(item));
+
+  expect(result).toEqual(expected);
+});
+
+test('testing Team.toArray function', () => {
+  const team = new Team();
+  const char1 = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  };
+  const char2 = {
+    name: 'лучник',
+    health: 8,
+    level: 2,
+    attack: 180,
+    defence: 20,
+  };
+  const char3 = {
+    name: 'маг',
+    health: 9,
+    level: 2,
+    attack: 160,
+    defence: 10,
+  };
+  const chars = [
+    char1,
+    char2,
+    char3,
+  ];
+  const expected = [
+    {
+      name: 'мечник',
+      health: 10,
+      level: 2,
+      attack: 80,
+      defence: 40,
+    },
+    {
+      name: 'лучник',
+      health: 8,
+      level: 2,
+      attack: 180,
+      defence: 20,
+    },
+    {
+      name: 'маг',
+      health: 9,
+      level: 2,
+      attack: 160,
+      defence: 10,
+    },
+  ];
+
+  team.addAll(chars);
+  team.toArray();
+
+  expect(team.arrayMembers).toEqual(expected);
+});
+
+test('testing ErrorRepository.translate function', () => {
+  const error = new ErrorRepository();
+  const result = error.translate(1);
+  expect(result).toBe('Ошибка ввода данных');
+});
+
+test('testing ErrorRepository.translate error', () => {
+  const error = new ErrorRepository();
+  const result = error.translate(777);
+  expect(result).toBe('Unknown error');
 });
--- src/js/Validator.js
+++
@@ -1,8 +0,0 @@
-export default class Validator {
-  validateUserName(userName) {
-    if (/\d{4}/.test(userName)) {
-      return false;
-    }
-    return /^[A-Za-z][\w-]*[A-Za-z]$/.test(userName);
-  }
-}
